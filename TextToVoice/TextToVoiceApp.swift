7818851e1d881764790c31df65879d59a4e71013
Nelson Cabrera <nelsoncabrera06@gmail.com>
2025-11-14 21:19:43 +0200

    Initial Commit

---
+++ TextToVoice/TextToVoiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  TextToVoiceApp.swift
+//  TextToVoice
+//
+//  Created by Nelson Cabrera on 14.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct TextToVoiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TextToVoiceTests/TextToVoiceTests.swift
@@ -0,0 +1,16 @@
+//
+//  TextToVoiceTests.swift
+//  TextToVoiceTests
+//
+//  Created by Nelson Cabrera on 14.11.2025.
+//
+
+import Testing
+
+struct TextToVoiceTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
